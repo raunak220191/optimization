988d81f9949855365ccc846a4caa0055cc24b2b9
raunak220191 <90885744+raunak220191@users.noreply.github.com>
2023-08-28 08:57:33 +0530

    Update OrTools_usage.py

--- OrTools_usage.py
+++ OrTools_usage.py
@@ -1,35 +1,51 @@
-from ortools.linear_solver import pywraplp
+from ortools.sat.python import cp_model
 
-solver = pywraplp.Solver.CreateSolver('SCIP')
+def optimize_scheduling(batches, num_ocr_processes, num_embed_processes):
+    model = cp_model.CpModel()
 
-num_batches = len(batches)
-num_processes = num_ocr_processes + num_embed_processes
+    num_batches = len(batches)
+    num_processes = num_ocr_processes + num_embed_processes
 
-x = {}
-for i in range(num_batches):
-    for j in range(num_processes):
-        x[i, j] = solver.IntVar(0, 1, f'x[{i},{j}]')
-
-# Add constraints
-for i in range(num_batches):
-    solver.Add(sum(x[i, j] for j in range(num_processes)) == 1)
-
-for j in range(num_processes):
-    solver.Add(sum(x[i, j] for i in range(num_batches)) == 1)
-
-# Define objective (minimize embedding)
-objective = solver.Objective()
-for i in range(num_batches):
-    objective.SetCoefficient(x[i, 1], 1)
-objective.SetMinimization()
-
-status = solver.Solve()
-
-if status == pywraplp.Solver.OPTIMAL:
+    # Binary variables to represent assignment of batches to processes
+    x = {}
     for i in range(num_batches):
         for j in range(num_processes):
-            if x[i, j].solution_value() == 1:
-                if j == 0:
-                    ocr_queue.put(batches[i])
-                else:
-                    embed_queue.put(batches[i])
+            x[i, j] = model.NewBoolVar(f'x_{i}_{j}')
+
+    # Each batch can only be assigned to one process
+    for i in range(num_batches):
+        model.Add(sum(x[i, j] for j in range(num_processes)) == 1)
+
+    # Embedding can start only after one batch of OCR is complete
+    for i in range(1, num_batches):
+        model.Add(sum(x[j, num_ocr_processes] for j in range(i)) >= x[i, num_ocr_processes])
+
+    # Objective: Minimize embeddings (embedding is heavier)
+    objective = sum(x[i, num_ocr_processes + 1] for i in range(num_batches))
+    model.Minimize(objective)
+
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+
+    if status == cp_model.OPTIMAL:
+        ocr_batches = []
+        embed_batches = []
+
+        for i in range(num_batches):
+            if solver.Value(x[i, num_ocr_processes]):
+                ocr_batches.append(batches[i])
+            else:
+                embed_batches.append(batches[i])
+
+        return ocr_batches, embed_batches
+
+    return None, None
+
+# Usage
+batches = [batch1, batch2, batch3, ...]
+num_ocr_processes = 5
+num_embed_processes = 5
+
+ocr_batches, embed_batches = optimize_scheduling(batches, num_ocr_processes, num_embed_processes)
+
+# Run OCR and embedding processes using ocr_batches and embed_batches
